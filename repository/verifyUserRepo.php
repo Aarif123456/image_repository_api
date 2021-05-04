bb01d3df2891b06aa3a8ce0b5cccf7a41c277933
Abdullah Arif <arif115@uwindsor.ca>
2021-05-04 02:23:48 -0400

    added repo code for verifying users

---
+++ repository/verifyUserRepo.php
@@ -0,0 +1,57 @@
+<?php
+
+/* Imports */
+require_once __DIR__ . '/error.php';
+
+function queryUserVerify($email, $userType, $conn) {
+    // get query
+    $query = getQueryForUserType($userType);
+    // if no query return back null
+    if (empty($query)) {
+        return null;
+    }
+    /* Prepare statement */
+    $stmt = $conn->prepare($query);
+    $stmt->bindValue(":email", $email, PDO::PARAM_STR);
+
+    // otherwise return back result from query
+    return getExecutedResult($stmt);
+}
+
+
+function getQueryForUserType($userType) {
+    $userVerifyQuery = '
+                            SELECT 
+                                id,
+                                firstName,
+                                lastName,
+                                password,
+                                \'user\' as userType 
+                            FROM 
+                                users
+                                INNER JOIN members ON id=memberID 
+                            WHERE 
+                                email = :email';
+    $adminVerifyQuery = "
+                            SELECT 
+                                id,
+                                firstName,
+                                lastName,
+                                password,
+                                'admin' as userType 
+                            FROM 
+                                users
+                                INNER JOIN members ON id=memberID 
+                                INNER JOIN professor ON memberID=adminID 
+                            WHERE 
+                                email = :email";
+    switch ($userType) {
+        case "user":
+            return $userVerifyQuery;
+        case "admin":
+            return $adminVerifyQuery;
+        default:
+            return '';
+    }
+}
+
