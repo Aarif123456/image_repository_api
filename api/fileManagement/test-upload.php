5a5c0c157479b0fb11d359a9857d26564b65607b
Abdullah Arif <arif115@uwindsor.ca>
2021-05-10 00:21:17 -0400

    small fixup

---
+++ api/fileManagement/test-upload.php
@@ -0,0 +1,5 @@
+<form method='post' action='upload.php' enctype='multipart/form-data'>
+  Send these files:<br />
+  <input type="file" name="images[]" id="file" multiple>
+  <input type='submit' value='Send files' />
+</form>
--- repository/encryption/decryptFile.php
+++ repository/encryption/decryptFile.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 require_once __DIR__ . '/encryptionConstants.php';
 require_once __DIR__ . '/callApi.php';
 
-function getFileDecrypted($encryptedFile, $user, $conn, $debug = false) {
+function getFileDecrypted($filePath, $fileName, $user, $conn, $debug = false) {
     $systemKeys = getSystemKeys($conn);
     $publicKey = $systemKeys['publicKey'];
     $privateKey = getUserKey($user, $conn);
-    
+    file_get_contents("$filePath/$fileName.Encrypted");
     return decrypt($publicKey, $privateKey, $encryptedFile, $debug);
 }
 
