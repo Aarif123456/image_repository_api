b5b2777d02ee9da1d7d5795740e710f3a3d71639
Abdullah Arif <arif115@uwindsor.ca>
2021-05-11 05:22:08 -0400

    added in endpoint to view image

---
+++ api/fileManagement/viewImage.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+/* Imports */
+require_once __DIR__ . '/../../views/apiReturn.php';
+require_once __DIR__ . '/../../views/errorHandling.php';
+require_once __DIR__ . '/../../common/constants.php';
+require_once __DIR__ . '/../../common/authenticate.php';
+require_once __DIR__ . '/../../repository/database.php';
+require_once __DIR__ . '/../../repository/viewImageRepo.php';
+require_once __DIR__ . '/../../repository/File.php';
+require_once __DIR__ . '/../../repository/User.php';
+
+/* Set required header and session start */
+requiredHeaderAndSessionStart();
+header('Content-Type: text/html; charset=UTF-8');
+
+if (empty($_REQUEST)) {
+    $_REQUEST = json_decode(file_get_contents('php://input'), true);
+}
+
+$debug = DEBUG;
+/* Connect to database */
+$conn = getConnection();
+
+/* Make sure user is logged in */
+if (!validateUser($conn)) {
+    redirectToLogin();
+}
+
+if (!(isValidRequestVar('fileName'))) {
+    throw new Exception(MISSING_PARAMETERS);
+}
+
+/* Set variables */
+$user = new User(getCurrentUserInfo($conn));
+$filePath = $_REQUEST['filePath'] ?? '';
+$fileName = $_REQUEST['fileName'];
+
+$file = new FileLocationInfo([
+    'name' => $fileName,
+    'path' => $filePath,
+    'ownerId' => $user->id
+]);
+
+$fileData = getImage($file, $user, $conn);
+$fileBinary = $fileData['data'];
+$mime = $fileData['mime'];
+
+echo '<img src="' . dataUri($fileBinary, $mime) . '" alt="you Image"/>';
+$conn = null;
+
+
+function dataUri($fileBinary, $mime): string {
+    return 'data:' . $mime . ';base64,' . base64_encode($fileBinary);
+}
