3d3fe5538cc44fe9507a54e6160535e2126c62e2
Abdullah Arif <arif115@uwindsor.ca>
2021-05-16 20:04:35 -0400

    added php docs

--- repository/encryption/callApi.php
+++ repository/encryption/callApi.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/encryptionConstants.php';
 require_once __DIR__ . '/encryptionExceptionConstants.php';
 /**
  * Modified: https://stackoverflow.com/questions/5647461/how-do-i-send-a-post-request-with-php
- * @throws Exception
+ * @throws EncryptionFailureException
  */
 function callApi(array $fields, array $options = [], bool $debug = false): array {
     $ch = curl_init();
@@ -41,7 +41,11 @@ function callApi(array $fields, array $options = [], bool $debug = false): array
     return $result;
 }
 
-/* Return properties used for encryption*/
+/**
+ * @param string $type
+ * @return array $properties used for encryption
+ * @throws EncryptionFailureException
+ */
 function generateProperties(string $type = 'a'): array {
     $args = [
         'method' => 'generateProperties',
@@ -51,7 +55,11 @@ function generateProperties(string $type = 'a'): array {
     return callApi($args);
 }
 
-/* Returns: $publicKey:string, $masterKey: string */
+/**
+ * @param string $properties
+ * @return array $publicKey:string, $masterKey: string
+ * @throws EncryptionFailureException
+ */
 function setup(string $properties = ENCRYPTION_PROPERTIES): array {
     $args = [
         'method' => 'setup',
@@ -61,7 +69,14 @@ function setup(string $properties = ENCRYPTION_PROPERTIES): array {
     return callApi($args);
 }
 
-/*Return: privateKey: string on success otherwise we get an error  */
+/**
+ * @param string $publicKey
+ * @param string $masterKey
+ * @param string $userAttributes
+ * @param string $properties
+ * @return string $privateKey: string on success otherwise we get an error
+ * @throws EncryptionFailureException
+ */
 function keygen(string $publicKey, string $masterKey, string $userAttributes, string $properties = ENCRYPTION_PROPERTIES): string {
     $args = [
         'method' => 'keygen',
@@ -75,7 +90,14 @@ function keygen(string $publicKey, string $masterKey, string $userAttributes, st
     throw new EncryptionFailureException('Keygen Failed!', 1);
 }
 
-/*Return: encryptedFile: string */
+/**
+ * @param string $publicKey
+ * @param string $policy
+ * @param string $inputFile
+ * @param string $properties
+ * @return string $encryptedFile: string
+ * @throws EncryptionFailureException
+ */
 function encrypt(string $publicKey, string $policy, string $inputFile, string $properties = ENCRYPTION_PROPERTIES): string {
     $args = [
         'method' => 'encrypt',
@@ -89,7 +111,14 @@ function encrypt(string $publicKey, string $policy, string $inputFile, string $p
     throw new EncryptionFailureException('Encrypt Failed!', 1);
 }
 
-/*Return: decryptedFile: string */
+/**
+ * @param string $publicKey
+ * @param string $privateKey
+ * @param string $encryptedFile
+ * @param string $properties
+ * @return string $decryptedFile: string
+ * @throws EncryptionFailureException
+ */
 function decrypt(string $publicKey, string $privateKey, string $encryptedFile, string $properties = ENCRYPTION_PROPERTIES): string {
     $args = [
         'method' => 'decrypt',
