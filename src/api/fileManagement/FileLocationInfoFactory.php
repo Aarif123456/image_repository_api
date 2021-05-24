e1c2acc859e8550ef1ec6abfb53c18e089bffead
aarif123456 <abdullahmeo11@gmail.com>
2021-05-23 22:09:53 -0400

    refactored function to create file object into a factory class

---
+++ src/api/fileManagement/FileLocationInfoFactory.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+namespace ImageRepository\api\FileManagement;
+
+use ImageRepository\Exception\{NoSuchFileException, StaticClassAssertionError};
+use ImageRepository\Model\{Database, FileLocationInfo, User};
+
+final class FileLocationInfoFactory
+{
+    private function __construct() {
+        throw new StaticClassAssertionError();
+    }
+
+    /**
+     * @throws NoSuchFileException
+     */
+    public static function createFromApiData(User $user, Database $db): FileLocationInfo {
+        $filePath = $_REQUEST['filePath'] ?? '/';
+        $fileName = $_REQUEST['fileName'] ?? '';
+        $fileId = $_REQUEST['fileId'] ?? null;
+        $file = null;
+        if (!empty($fileId)) {
+            $file = FileLocationInfo::createFromId($fileId, $user, $db);
+        } elseif (!empty($fileName)) {
+            $file = new FileLocationInfo([
+                'name' => $fileName,
+                'path' => $filePath,
+                'ownerId' => $user->id
+            ]);
+        }
+
+        return $file;
+    }
+}
